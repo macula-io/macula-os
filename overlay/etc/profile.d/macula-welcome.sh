d9f213e1adb799b9a9795b8fa9f975e1ac4891bf
beamologist <raf.lefever@erlef.org>
2026-01-12 00:13:35 +0100

    fix(branding): correct banner alignment
    Align mesh logo lines properly with figlet text:
    - Logo line 1: standalone
    - Logo lines 2-6: aligned with figlet text lines 1-5
    - Logo line 7: standalone
    - Tagline below
    
    Previous version had misaligned spacing between logo and text.

--- overlay/etc/profile.d/macula-welcome.sh
+++ overlay/etc/profile.d/macula-welcome.sh
@@ -16,14 +16,14 @@ if command -v fastfetch >/dev/null 2>&1; then
     fastfetch --config /etc/macula/fastfetch.jsonc 2>/dev/null
 else
     printf '\n'
-    printf '%s    .o---o.%s\n' "$C" "$R"
-    printf '%s   /|     |\\%s       %s __  __    _    ____ _   _ _        _        ___  ____  %s\n' "$C" "$R" "$O" "$R"
-    printf '%s  o-+-----+-o%s     %s|  \/  |  / \  / ___| | | | |      / \      / _ \/ ___| %s\n' "$C" "$R" "$O" "$R"
-    printf '%s   \\|     |/%s      %s| |\/| | / _ \| |   | | | | |     / _ \    | | | \___ \ %s\n' "$C" "$R" "$O" "$R"
-    printf "%s    'o---o'%s       %s| |  | |/ ___ \ |___| |_| | |___ / ___ \   | |_| |___) |%s\n" "$C" "$R" "$O" "$R"
-    printf '%s      \\ /%s         %s|_|  |_/_/   \_\____|\___/|_____/_/   \_\   \___/|____/ %s\n' "$C" "$R" "$O" "$R"
-    printf '%s       o%s\n' "$C" "$R"
-    printf '                  %sDecentralized Edge Computing Platform%s\n' "$W" "$R"
+    printf '%s    o-------o%s\n' "$C" "$R"
+    printf '%s   /|       |\\%s        %s __  __    _    ____ _   _ _        _        ___  ____ %s\n' "$C" "$R" "$O" "$R"
+    printf '%s  o-+--o----+-o%s     %s|  \\/  |  / \\  / ___| | | | |      / \\      / _ \\/ ___| %s\n' "$C" "$R" "$O" "$R"
+    printf '%s   \\|\\ | /| /%s       %s| |\\/| | / _ \\| |   | | | | |     / _ \\    | | | \\___ \\ %s\n' "$C" "$R" "$O" "$R"
+    printf "%s    o--O--o%s         %s| |  | |/ ___ \\ |___| |_| | |___ / ___ \\   | |_| |___) |%s\n" "$C" "$R" "$O" "$R"
+    printf '%s   /|  |  |\\%s        %s|_|  |_/_/   \\_\\____|\\___/|_____/_/   \\_\\   \\___/|____/ %s\n' "$C" "$R" "$O" "$R"
+    printf '%s  o----o----o%s\n' "$C" "$R"
+    printf '                    %sDecentralized Edge Computing Platform%s\n' "$W" "$R"
     printf '\n'
     printf '%s  Hostname: %s%s\n' "$G" "$(hostname)" "$R"
     printf '%s  Uptime:   %s%s\n' "$G" "$(uptime -p 2>/dev/null || uptime)" "$R"
