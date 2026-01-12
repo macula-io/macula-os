62ba525e735b42145fc394d447a96c637e05deec
beamologist <raf.lefever@erlef.org>
2026-01-12 01:56:25 +0100

    fix(branding): shift bottom frame border 1 right

--- overlay/etc/profile.d/macula-welcome.sh
+++ overlay/etc/profile.d/macula-welcome.sh
@@ -19,7 +19,7 @@ else
     printf '%s    o-------o%s\n' "$C" "$R"
     printf '%s   /|       |\\%s      %s+========================+%s\n' "$C" "$R" "$O" "$R"
     printf '%s  o-+--o----+-o%s     %s||%s%s      Macula OS       %s%s||%s\n' "$C" "$R" "$O" "$R" "$W" "$R" "$O" "$R"
-    printf '%s   \\|\\ | /| /%s      %s+========================+%s\n' "$C" "$R" "$O" "$R"
+    printf '%s   \\|\\ | /| /%s       %s+========================+%s\n' "$C" "$R" "$O" "$R"
     printf '%s    o--O--o%s\n' "$C" "$R"
     printf '%s   /|  |  |\\%s        %sDecentralized Edge Computing Platform%s\n' "$C" "$R" "$W" "$R"
     printf '%s  o----o----o%s\n' "$C" "$R"
